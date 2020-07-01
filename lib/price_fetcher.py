76d2ec150765250f9059270f35845eca313831ae
Corbin Graham <corbin.graham2@gmail.com>
2020-07-01 13:12:54 -0700

    Updated the price fetcher to scrape HTML directly rather than using the very unstable API provided

--- lib/price_fetcher.py
+++ lib/price_fetcher.py
@@ -1,13 +1,13 @@
 import requests
 import time
 import copy
+import re
 
-# This file fetches all of the pricing data from the OSRS GE API.
-#https://www.reddit.com/r/2007scape/comments/3g06rq/guide_using_the_old_school_ge_page_api/
+# This file fetches all of the pricing data from the OSRS GE HTML pages.
+# Priceing data is scraped for the available 180 days of data.
+# HTML is used over the API as the API response rate and burst rates are low and unstable.
 
-# Need to modify this to scrape directly from the osrs 
-# http://services.runescape.com/m=itemdb_oldschool/Zulrah%27s+scales/viewitem?obj=12934
-BASE_URL = 'http://services.runescape.com/m=itemdb_oldschool'
+BASE_URL = 'http://services.runescape.com/m=itemdb_oldschool/'
 class price_fetcher:
 
 	# Need to add checks to make sure that each of the pages return data.
@@ -17,51 +17,50 @@ class price_fetcher:
 		for item_id in ids_and_items:
 			successful = False
 			while not successful:
-				import pdb; pdb.set_trace()
-				url = BASE_URL + '/api/graph/' + str(item_id) + '.json'  
+				#import pdb; pdb.set_trace()
+				url = BASE_URL + ids_and_items[item_id].replace(' ', '+') + '/viewitem?obj=' + str(item_id) 
 				print(item_id)
 				
-				# Try to fetch price data from API
+				# Try to fetch raw HTML from osrs ge item pages.
 				try:
-					# If we can't convert to a JSON, the response is empty.
+					# If we can't read the text, the response is empty.
 					page = requests.get(url)
 
-					# if we hit a 404 error don't keep attempting the item.
+					# if we hit a 404 error don't keep attempting the item as it likely does not exist.
 					if page.status_code == 404:
 						break
-					page = page.json()
+					page = page.text
 
 				except:
 					# Sleep longer because we failed
-					print('failed on' + ids_and_items[item_id])
-					time.sleep(20)
+					print('failed on ' + ids_and_items[item_id])
+					time.sleep(5)
 					continue
 				
 
 				successful = True
-				page = self.convert_to_dates(page)
+
+				page = self.parse_html(page)
 				
 				id_with_pricing_info.update([(item_id, page)])
-
-				# Sleep, so we don't get timed out
-				time.sleep(0.5)				
+				
+				# Not sleeping here since each HTML request takes ~2 seconds anyways.				
 
 		return id_with_pricing_info
 
-	# Times are all presented in Epoch from the API
-	# This method converts them to YYYY-MM-dd
-	def convert_to_dates(self, item_json):
-		# We aren't using the 'daily' data so we are just going to pop them from the JSON
-		item_json.pop('daily', None)
+	# Accepts the HTML page in its raw form and returns a dict of form [date: price_on_date, date2: price_on_date2]
+	def parse_html(self, page):
+		# A regex to find all of the price points over the past 180 days
+		list_of_180_price_points = re.findall(r'average180.push.*;', page)
+		dates_to_price = dict()
 
-		# Update the keys from an Epoch time to form 'YYYY-mm-dd'
-		for epoch_time in item_json['average']:
-			converted_date = time.strftime('%Y-%m-%d', time.localtime(int(epoch_time[0:-3])))
-			item_json[converted_date] = item_json['average'][epoch_time]
+		for price_point in list_of_180_price_points:
+			date = price_point[27:37].replace('/', '-') # Conforming to date style of YYYY-MM-dd
 
-		# Simplifies the JSON since we've updated the keys
-		item_json.pop('average', None)
-			
-		return item_json
+			price_point_split = price_point.split(',')
+			daily_price = price_point_split[1].strip(' ') # Not using daily price right now but will likely add in future
+			moving_average_price = int(price_point_split[2][1:-3]) # Strips extra characters
 
+			dates_to_price.update([(date, moving_average_price)])
 
+		return dates_to_price
--- lib/read_item_ids.py
+++ lib/read_item_ids.py
@@ -1,12 +1,14 @@
 path = '../bin/tradable_items.txt'
 
+# Input: File containing items names and ids of form (id: name)
+# Output: dict() of form {id: name, id2: name2}
 # Reads in text file containing ids and tradeable items
 def read_item_ids():
 	item_id_dic = dict()
 	with open(path, 'r') as file:
 	    for line in file:
 	    	line = line.strip()
-	    	if line == '':
+	    	if line == '': # skip any empty rows
 	    			continue
 	    	split_line = line.split(':')
 	    	item_id_dic.update([(int(split_line[0]), split_line[1][1:])])
